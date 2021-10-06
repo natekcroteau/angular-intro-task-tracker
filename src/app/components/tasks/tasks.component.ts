13bb6be3f8da58ee497033c80426f668d2c3b7be
natekcroteau <natekcroteau@gmail.com>
2021-10-06 10:19:11 -0600

    setup json server for temporary backend

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http'
 import { Task } from '../../Task'
 import { TaskService } from '../../services/task.service'
 
