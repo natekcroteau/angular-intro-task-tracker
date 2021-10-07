eac49d3a0a2aeadb880eeb8435ce6fe9bd1fb7fc
natekcroteau <natekcroteau@gmail.com>
2021-10-07 13:23:05 -0600

    updated code formatting

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -13,7 +13,7 @@ const httpOptions = {
   providedIn: 'root'
 })
 export class TaskService {
-  private apiURL = 'http://localhost:5000/tasks'
+  private apiURL = 'http://localhost:5000/tasks';
 
   constructor(private http:HttpClient) { }
 
